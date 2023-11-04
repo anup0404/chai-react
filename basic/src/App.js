b252ea709d1aaac00e536ab48ebf89a6aefc7474
Anup Kumar <anupkumarggupta@gmail.com>
2023-11-04 20:16:16 +0530

    refcopy revision

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,7 +1,16 @@
+import { useCallback, useRef } from "react";
+
 function App() {
+  const refCopy = useRef(null);
+  const namecopy = useCallback(() => {
+    refCopy.current?.select();
+    alert("password Copy");
+  }, []);
   return (
     <div className="App">
       <h1>Chai aur Code </h1>
+      <input type="text" placeholder="Enter your name" ref={refCopy} />
+      <button onClick={namecopy}>copy </button>
     </div>
   );
 }
